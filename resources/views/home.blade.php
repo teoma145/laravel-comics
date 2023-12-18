00c77d541dd7437d2319344194312605bbf52ca2
teoma145 <matteodark145@yahoo.it>
2023-12-18 17:10:40 +0100

    cards-current-series

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,19 @@
 @section('title','Home')
 
 @section('content')
- <main>
-    <section class="container">
-        <h1>comics</h1>
+ <main class="cardsbg">
+    <section id="cardscontent" class="container cardspop">
+        <div class="bg-info currentser">
+            <h3 class="text-white fs-4">CURRENT SERIES</h3>
+        </div>
+
         <div class="row">
             @foreach ($comics as $comic)
-            <div class="col-12 col-md-4  col-lg-3">
+            <div class="col-12 col-md-4  col-lg-2 mt-5">
                 <div class="cards">
                     <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
                 </div>
+                <h5 class="card-title text-white  fs-6">{{$comic['title']}}</h5>
             </div>
             @endforeach
         </div>
